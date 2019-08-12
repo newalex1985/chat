2a75fe6c09592c01a9090ef6a0273692f4aad78b
Aliaksandr Konan <newalex1985@tut.by>
2019-08-12 23:20:55 +0300

    feat: implementing connection to server

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -10,15 +10,72 @@ export default class App extends Component {
   
   constructor() {
     super();
-    
+    // this.tempId = 100;
+    this.socket = '';
     this.state = {
-      
+      messages: [
+          // {from: 'Nick', message: 'Test message1', id: 1, time: 0},
+          // {from: 'Andy', message: 'Test message2', id: 2, time: 0},
+          // {from: 'Marta', message: 'Test message3', id: 3, time: 0}
+      ]
     };
     this.sendMessage = this.sendMessage.bind(this);
   }
 
+  // createOwnMessage(text) {
+  //     return {
+  //         from: 'Me', message: text, id: this.tempId++, time: 0
+  //     };
+  // }
+
   sendMessage(text) {
-    console.log(text);
+    // const ownMessage = this.createOwnMessage(text);
+    this.socket.send(JSON.stringify({ from: 'Alex', message: text }));
+    // this.setState(({ messages }) => {
+    //     return {
+    //         messages: [...messages, ownMessage]
+    //     };
+    // });
+  }
+
+  receiveMessages(data) {
+    const newMessages = JSON.parse(data).map(({ from, message, id, time }) => {
+      return {
+        from,
+        message,
+        id,
+        time
+      };
+    });
+    console.log(data);
+    // console.log(JSON.parse(data));
+    // console.log(newMessages);
+    this.setState(({ messages }) => {
+      return {
+        messages: [...messages, ...newMessages]
+      };
+    });
+  }
+
+  componentDidMount() {
+    this.socket = new WebSocket('ws://st-chat.shas.tel');
+    this.socket.binaryType = "arraybuffer";
+    this.socket.onopen = function(event) {console.log('connect');};
+    
+    this.socket.onmessage = (event) => {
+      this.receiveMessages(event.data);
+    };
+
+    this.socket.onerror = function(error) {console.log(error.message);};
+
+    this.socket.onclose = function(event) {
+      if (event.wasClean) {
+        console.log('Соединение закрыто чисто');
+      } else {
+        console.log('Обрыв соединения');
+      }
+      console.log('Код: ' + event.code + ' причина: ' + event.reason);
+    };
   }
 
   render() {
@@ -26,7 +83,8 @@ export default class App extends Component {
     return (
       <div className="chat-app">
         <Title />
-        <MessageList />
+        <MessageList
+         messages={this.state.messages}/>
         <SendMessageForm 
             sendMessage={this.sendMessage}/>
       </div>
--- src/components/message-list/message-list.js
+++ src/components/message-list/message-list.js
@@ -4,11 +4,18 @@ import './message-list.css';
 
 export default class MessageList extends Component {
   render() {
+    const { messages } = this.props;
+    const mesagesList = messages.map((message) => {
+      return (
+        <li key={message.id} className="message">
+          <div><span className="author-message">{message.from}</span></div>
+          <div><span className="text-message">{message.message}</span></div>
+        </li>
+      );
+    });
     return (
       <ul className="message-list">                 
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
+        {mesagesList}
      </ul>
     )
   }
